246235b49e2a9843cb6313fd47f61daad2bc7958
AminMahjub <aminmahjoob3172@gmail.com>
2023-10-29 10:51:29 +0330

    Add no todos icon to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import useCategory from "./hooks/useCategory";
 
 function App() {
   const [count, setCount] = useState(0);
